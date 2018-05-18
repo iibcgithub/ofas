bd9ddfa35d4872206ec8fe2851e84d9510d3fdd1
iibcgithub <39420518+iibcgithub@users.noreply.github.com>
2018-05-18 15:09:11 -0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,175 @@
+/*
+Author       : theme_ocean
+Template Name: Kothari - App Landing Page
+Version      : 1.0
+*/
+(function($) {
+	'use strict';
+	
+	jQuery(document).on('ready', function(){
+	
+		/*PRELOADER JS*/
+		$(window).on('load', function() {  
+			$('.spinner').fadeOut();
+			$('.preloader').delay(350).fadeOut('slow'); 
+		}); 
+		/*END PRELOADER JS*/		
+		
+		/*START MENU JS*/		
+			 if ($(window).scrollTop() > 200) {
+              $('.fixed-top').addClass('menu-bg');
+          } else {
+              $('.fixed-top').removeClass('menu-bg');
+          }
+			$(window).on('scroll', function(){
+				if ( $(window).scrollTop() > 100 ) {
+					$('.site-navigation, .header-white, .header').addClass('navbar-fixed');
+				} else {
+					$('.site-navigation, .header-white, .header').removeClass('navbar-fixed');
+				}
+			});				
+		/*END MENU JS*/								
+
+		/* START SCREENSHOTS SLIDER JS*/
+		$('.s-slider').owlCarousel({ 
+			autoPlay: 10000, //Set AutoPlay to 4 seconds	 
+			items : 4,
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,3],
+			itemsTablet : [768,2],
+			itemsMobile	: [479,1],
+			pagination	: true,
+		});
+		/* END START SCREENSHOTS SLIDER JS*/	
+		
+		/*START VIDEO JS*/
+		$('.video-play').magnificPopup({
+            type: 'iframe'
+        });
+		/*END VIDEO JS*/
+
+
+		/*START HOME WATER JS*/
+		  if (typeof $.fn.ripples == 'function') {
+				try {
+					$('.ripple').ripples({
+						resolution: 500,
+						perturbance: 0.01
+					});
+				} catch (e) {
+					$('.error').show().text(e);
+				}
+			}
+		/*END HOME WATER JS*/
+		
+		/* START COUNTDOWN JS*/
+		$('.counter_feature').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+			if (visible) {
+				$(this).find('.counter-num').each(function () {
+					var $this = $(this);
+					$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+						duration: 2000,
+						easing: 'swing',
+						step: function () {
+							$this.text(Math.ceil(this.Counter));
+						}
+					});
+				});
+				$(this).unbind('inview');
+			}
+		});
+		/* END COUNTDOWN JS */	
+
+		/*START TEAM JS*/	
+		$("#single_team_slider").owlCarousel({
+		   items:3,
+			itemsDesktop:[1000,3],
+			itemsDesktopSmall:[980,2],
+			itemsTablet:[768,2],
+			itemsMobile:[650,1],
+			pagination:true,
+			navigation:false,
+			slideSpeed:1000,
+			autoPlay:false
+		});
+		/*END TEAM JS*/
+		
+		/*START TESTIMONIAL*/
+		$("#testimonial-slider").owlCarousel({
+			items:2,
+			itemsDesktop:[1000,2],
+			itemsDesktopSmall:[979,2],
+			itemsTablet:[768,1],
+			pagination:true,
+			navigation:false,
+			autoplay:false
+		});
+		/*END TESTIMONIAL*/
+
+		/*START BLOG SLIDER*/
+		$("#news-slider").owlCarousel({
+			items : 1,
+			itemsDesktop:[1199,2],
+			itemsDesktopSmall:[980,1],
+			itemsTablet: [600,1],
+			itemsMobile : [550,1],
+			pagination:true,
+			autoPlay:false
+		});
+		/*END BLOG SLIDER*/
+
+		/*START PARTNER LOGO*/
+		$('.partner').owlCarousel({
+		  autoPlay: 5000, //Set AutoPlay to 5 seconds
+		  items : 5,
+		  itemsDesktop : [1199,3],
+		  itemsDesktopSmall : [979,3]
+		});
+		/*END PARTNER LOGO*/
+		
+		/*START GOOGLE MAP*/
+		function initialize() {
+		  var mapOptions = {
+			zoom: 15,
+			scrollwheel: false,
+			center: new google.maps.LatLng(-37.7622470161679, 145.06004333496094)
+		  };
+		  var map = new google.maps.Map(document.getElementById('map'),
+			  mapOptions);
+		  var marker = new google.maps.Marker({
+			position: map.getCenter(),
+			icon: 'assets/img/map_pin.png',
+			map: map
+		  });
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);	
+		/*END GOOGLE MAP*/		
+		
+		
+	}); 			
+	
+	/* START PARALLAX JS */
+	(function () {
+
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		 
+		} else {
+			$(window).stellar({
+				horizontalScrolling: false,
+				responsive: true
+			});
+		}
+
+	}());
+	/* END PARALLAX JS  */	
+	
+	/*START WOW ANIMATION JS*/
+	  new WOW().init();	
+	/*END WOW ANIMATION JS*/	
+
+	
+})(jQuery);
+
+
+  
+
